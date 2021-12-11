45bf49b97608195ef068882356173ef132dbc350
MIDHUnRaj647 <midhunraj647@gmail.com>
2021-12-11 16:33:00 +0530

    upload2 11122021

---
+++ TheEvent/assets/js/email.js
@@ -0,0 +1,18 @@
+function sendEmail() {
+  var from;
+  var subject;
+  var body;
+  from=document.querySelector(".form-group col-md-6 mt-3 mt-md-0 .form-control")
+  Email.send({
+    Host: "smtp.gmail.com",
+    Username: "dhegodcoder@gmail.com",
+    Password: "osbusessymviqovs",
+    To: 'dhegodcoder@gmail.com',
+    From: "abijithks266@gmail.com",
+    Subject: "Sending Email using javascript",
+    Body: "Well that was easy!!",
+  })
+        .then(function (message) {
+          alert("mail sent successfully")
+        });
+    }
