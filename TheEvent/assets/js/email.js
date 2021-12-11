2ec581c267072904513cf3178a59ee16d9c427c4
Midhun Raj M <71880031+MIDHUnRaj647@users.noreply.github.com>
2021-12-11 20:00:16 +0530

    update2

--- TheEvent/assets/js/email.js
+++ TheEvent/assets/js/email.js
@@ -2,16 +2,18 @@ function sendEmail() {
   var from;
   var subject;
   var body;
-  from=document.querySelector(".form-group col-md-6 mt-3 mt-md-0 .form-control")
-  subject=
+  from=document.getElementById("email").value;
+  subject=document.getElementById("subject").value;
+  body=document.getElementById("body").value;
+  console.log(subject,from,body)
   Email.send({
     Host: "smtp.gmail.com",
     Username: "dhegodcoder@gmail.com",
     Password: "osbusessymviqovs",
     To: 'dhegodcoder@gmail.com',
-    From: "midhunraj647@gmail.com",
+    From: from,
     Subject: subject,
-    Body: "Well that was easy!!",
+    Body: body,
   })
         .then(function (message) {
           alert("mail sent successfully")
